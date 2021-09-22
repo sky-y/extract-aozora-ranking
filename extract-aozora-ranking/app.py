7feb0740c9c931f25628b4bb7e42724a66569ec7
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-09-22 11:34:58 +0900

    [update] コンボボックスにPythonから変数を与える

--- extract-aozora-ranking/app.py
+++ extract-aozora-ranking/app.py
@@ -9,16 +9,26 @@ def index():
     # 時期
     select_date = request.form.get('select_date')
 
-    # ランキング種別
-    select_type = request.form.get('select_type')
+    # # ランキング種別
+    # select_type = request.form.get('select_type')
 
     print("date:", select_date)
-    print("type:", select_type)
+    # print("type:", select_type)
+
+    dic_dates = {
+        '2021-06-01': '2021年6月',
+        '2021-05-01': '2021年5月',
+        '2021-04-01': '2021年4月',
+        '2021-03-01': '2021年3月',
+    }
+
+    default_date = '2021-05-01'
 
     return render_template('index.html',
                            title=title,
                            select_date=select_date,
-                           select_type=select_type)
+                           dic_dates=dic_dates,
+                           default_date=default_date)
 
 # おまじない
 if __name__ == "__main__":
